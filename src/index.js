570386f34bb21240a068515d79b7215214310ced
hoanguyen6611 <hoahuy2606.com>
2025-07-26 12:22:02 +0700

    update router

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import categoryRouter from "./routes/category.route.js";
 import tagRouter from "./routes/tag.route.js";
 import notificationRouter from "./routes/notification.routes.js";
 import webhookRouter from "./routes/webhook.route.js";
+import mainRouter from "./routes/index.js";
 import connectDB from "./lib/connectDB.js";
 import { clerkMiddleware } from "@clerk/express";
 import cors from "cors";
@@ -15,7 +16,7 @@ const app = express();
 
 app.use(cors(process.env.CLIENT_URL));
 app.use(clerkMiddleware());
-app.use("/webhooks", webhookRouter);
+// app.use("/webhooks", webhookRouter);
 app.use(express.json());
 
 app.use(function (req, res, next) {
@@ -27,12 +28,13 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.use("/posts", postRouter);
-app.use("/users", userRouter);
-app.use("/comments", commentRouter);
-app.use("/tags", tagRouter);
-app.use("/category", categoryRouter);
-app.use("/notifications", notificationRouter);
+// app.use("/posts", postRouter);
+// app.use("/users", userRouter);
+// app.use("/comments", commentRouter);
+// app.use("/tags", tagRouter);
+// app.use("/category", categoryRouter);
+// app.use("/notifications", notificationRouter);
+app.use("/", mainRouter);
 
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
